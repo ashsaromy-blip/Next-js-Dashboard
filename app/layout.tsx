d2e5c1234c9dddf0967b835f76a4b916f44799cf
ashsaromy-blip <ashsaromy@gmail.com>
2026-09-07 11:26:42 +0530

    Initial dashboard project

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+       <body className={`${inter.className} antialiased`}>{children}
+ <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    ></p>
+    </body>
+    </html>
+  );
+}
